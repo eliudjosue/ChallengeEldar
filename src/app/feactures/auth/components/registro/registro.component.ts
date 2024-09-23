58d9ee6ef0cbe536721e66de00dee68119beb235
eliudjc <ecampos@coasa.com.ar>
2024-09-23 14:44:19 -0300

    fix

--- src/app/feactures/auth/components/registro/registro.component.ts
+++ src/app/feactures/auth/components/registro/registro.component.ts
@@ -72,24 +72,18 @@ export class RegistroComponent {
     });
   }
 
-  // Este método se ejecutará cuando el formulario se envíe
   onSubmit() {
     if (this.password !== this.confirmPassword) {
       console.error('Las contraseñas no coinciden');
       return;
     }
 
-    // Lógica para registrar al usuario (ej. llamada a un servicio backend)
-    console.log('Nombre:', this.name);
-    console.log('Email:', this.email);
-    console.log('Contraseña:', this.password);
 
 this.Registrar(this.email, this.password)
   }
 
   Registrar(email: string, password: string) {
     this.authService.register(email, password).then(res => {
-      console.log("Se registro :", res)
       if (res?.operationType == 'signIn') {
         this.router.navigate(['/productos']);
       }
